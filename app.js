261d1bf6af6858392a6408bf8c2e5ad235d14405
smit659 <smittsoni659@gmail.com>
2021-08-01 20:23:15 +0530

    process.env.PORT mistake solved

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.post("/",function(req,res){
 
   });
 
-app.listen(process.env.PORT,function(){
-console.log("Listening on port 3100");
+app.listen(process.env.PORT||3000,function(){
+console.log("Listening on port 3000");
 });
 
